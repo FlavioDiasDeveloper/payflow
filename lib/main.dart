69c9a05f0f51994bd4a238da9bdf31a6f5294847
Flavio Dias <84815157+FlavioDiasDeveloper@users.noreply.github.com>
2022-05-18 22:33:12 -0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,44 @@
+import 'package:flutter/material.dart';
+
 void main() {
-  print('Hellow WOrd');
+  runApp(MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.purple),
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int contador = 0;
+  void increment() {
+    contador++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pay Flow'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Text(
+        'Contador\n $contador',
+        textAlign: TextAlign.center,
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
+    );
+  }
 }
